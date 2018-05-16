49e0d1c9e2afe7b59cf9e33d3e2ed4339d0c1d52
pkrajewski <piotrkrajewski55@gmail.com>
2018-05-16 17:37:48 +0200

    fix

--- src/main/java/my/initial/project/selenium/jenkins/TestRunner.java
+++ src/main/java/my/initial/project/selenium/jenkins/TestRunner.java
@@ -1,12 +1,11 @@
 package my.initial.project.selenium.jenkins;
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+
+
 @CucumberOptions(
 		features = {"feature/","classpath:my.initial.project.selenium.jenkins.firstTests"},
 		format = {"pretty","json:target/cucumber.json"}
