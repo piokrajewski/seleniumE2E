65f000bd997aedb1a50512ff27a6bbd8abeb8de2
Janek Góral <janek.goral@gogoapps.io>
2018-05-17 00:57:08 +0200

    add test for headless firefox

--- src/main/java/my/initial/project/selenium/jenkins/headless.java
+++ src/main/java/my/initial/project/selenium/jenkins/headless.java
@@ -1,17 +1,48 @@
 package my.initial.project.selenium.jenkins;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class headless {
 
+    @Test
+    public void runFirefoxHeadless() throws IOException {
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
+        try {
+            driver.get("http://www.google.com");
+            driver.manage().timeouts().implicitlyWait(4,
+                    TimeUnit.SECONDS);
+            WebElement queryBox = driver.findElement(By.name("q"));
+            queryBox.sendKeys("headless firefox");
+            WebElement searchBtn = driver.findElement(By.name("btnK"));
+            searchBtn.click();
+            WebElement iresDiv = driver.findElement(By.id("ires"));
+            iresDiv.findElements(By.tagName("a")).get(0).click();
+            System.out.println(driver.getPageSource());
+        } finally {
+            driver.quit();
+        }
+    }
 
     @Test
+    @Ignore
     public void createChromeDriverHeadless() throws IOException {
         ChromeOptions chromeOptions = new ChromeOptions();
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
