2800069946e19f3a8a297eb47c3cdab9cbb68113
pkrajewski <piotrkrajewski55@gmail.com>
2018-05-16 20:45:02 +0200

    fix

--- src/main/java/my/initial/project/selenium/jenkins/test.java
+++ src/main/java/my/initial/project/selenium/jenkins/test.java
@@ -1,15 +1,21 @@
 package my.initial.project.selenium.jenkins;
 
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class test {
 	@Test
 	public void test() {
-		System.setProperty("webdriver.chrome.driver", "/usr/local/bin");
-		WebDriver driver = new ChromeDriver(); //
+		//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no--sandbox");
+		WebDriver driver = new ChromeDriver(options); 
 		driver.get("https://www.google.pl");
+		String actual = driver.getTitle();
+		Assert.assertEquals("Google", actual);
 	}
 }
