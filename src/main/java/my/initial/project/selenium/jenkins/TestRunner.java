2c5d4bb3c38747b8361a8ceeba72fe15abd3e51f
pkrajewski <piotrkrajewski55@gmail.com>
2018-05-16 12:17:42 +0200

    repair

--- src/main/java/my/initial/project/selenium/jenkins/TestRunner.java
+++ src/main/java/my/initial/project/selenium/jenkins/TestRunner.java
@@ -3,19 +3,17 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-import org.junit.Test;
+
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"feature/"},
-		format = {"pretty","json:target/cucumber.json"}
+		format = {"pretty","json:target/cucumber.json"},
+		glue="classpath:my.initial.project.selenium.jenkins.StepDefinitions"
 		)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-	@Test
-	public void test() {
-		throw new RuntimeException();
-	}
+	
 	
 }
