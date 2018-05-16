1eb9777431f5606f2d2980033670c979caaf3086
Janek Góral <janek.goral@gogoapps.io>
2018-05-16 23:48:44 +0200

    add chrome options arguments

--- src/main/java/my/initial/project/selenium/jenkins/headless.java
+++ src/main/java/my/initial/project/selenium/jenkins/headless.java
@@ -11,19 +11,22 @@ import java.io.IOException;
 public class headless {
 
 
-	  @Test
-	  public void createChromeDriverHeadless() throws IOException
-	  {
-	    ChromeOptions chromeOptions = new ChromeOptions();
-	    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-	    chromeOptions.setBinary("/usr/bin/google-chrome");
-	    chromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-	    chromeOptions.addArguments("--disable-gpu");
-	    WebDriver driver = new ChromeDriver(chromeOptions);
-	    driver.navigate().to("https://www.facebook.com");
-	    String actual = driver.getTitle();
-	    Assert.assertEquals("Google", actual);
-	    driver.quit();
-	  }
+    @Test
+    public void createChromeDriverHeadless() throws IOException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        chromeOptions.setBinary("/usr/bin/google-chrome");
+        chromeOptions.addArguments(
+                "--headless",
+                "--no-sandbox",
+                "--disable-gpu",
+                "--remote-debugging-port=9222"
+        );
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.navigate().to("https://www.facebook.com");
+        String actual = driver.getTitle();
+        Assert.assertEquals("Google", actual);
+        driver.quit();
+    }
 
 }
