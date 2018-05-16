37a966fc5510e423d44d69e214636e25b53a5bce
pkrajewski <piotrkrajewski55@gmail.com>
2018-05-16 20:51:36 +0200

    fix

--- src/main/java/my/initial/project/selenium/jenkins/StepDefinitions.java
+++ src/main/java/my/initial/project/selenium/jenkins/StepDefinitions.java
@@ -4,6 +4,7 @@ package my.initial.project.selenium.jenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -15,7 +16,10 @@ public class StepDefinitions {
 	
 	@When("^I open browser$")
 	public void i_open_browser() throws Throwable {
-	    WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no--sandbox");
+		WebDriver driver = new ChromeDriver(options); 
 	    driver.get("https://www.google.pl");
 	}
 
